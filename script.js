5d5643cf0fa0293b0ce9dae9251bd61bfeb265ee
Davidtt083 <92117303+Davidtt083@users.noreply.github.com>
2023-07-11 15:16:06 -0600

    si

--- script.js
+++ script.js
@@ -40,39 +40,68 @@ jQuery(window).resize(function () {
 	makeResizable('#very-specific-design');
 });
 
-
-function mostrarp(divNum,btns){
+function mostrarp(divNum, btns, lot) {
 	document.getElementById("text1").classList.add("hide");
 	document.getElementById("text2").classList.add("hide");
 	document.getElementById("text3").classList.add("hide");
 	document.getElementById("text4").classList.add("hide");
-	document.getElementById("text5").classList.add("hide");
-	document.getElementById("text6").classList.add("hide");
-	document.getElementById("text7").classList.add("hide");
-	document.getElementById("text8").classList.add("hide");
-	document.getElementById("text9").classList.add("hide");
-	document.getElementById("text10").classList.add("hide");
-	document.getElementById("text11").classList.add("hide");
 
-	document.getElementById("btn1").classList.remove("activo");
+	document.getElementById("lot1").classList.add("hide");
+	document.getElementById("lot2").classList.add("hide");
+	document.getElementById("lot3").classList.add("hide");
+	document.getElementById("lot4").classList.add("hide");
+
+
 	document.getElementById("btn2").classList.remove("activo");
 	document.getElementById("btn3").classList.remove("activo");
 	document.getElementById("btn4").classList.remove("activo");
 	document.getElementById("btn5").classList.remove("activo");
-	document.getElementById("btn6").classList.remove("activo");
 
-  
+
+
 	// Show the specific div
 	document.getElementById("text" + divNum).classList.remove("hide");
 	document.getElementById("btn" + btns).classList.add("activo");
+	document.getElementById("lot" + lot).classList.remove("hide");
 }
 
-function cerrar(){
-	document.getElementById("text1").classList.add("hide");
-	document.getElementById("text2").classList.add("hide");
-	document.getElementById("text3").classList.add("hide");
-	document.getElementById("text4").classList.add("hide");
-	document.getElementById("text5").classList.add("hide");
-	document.getElementById("text6").classList.add("hide");
-	document.getElementById("text7").classList.add("hide");
-}
+
+// Cargar la primera animación en el primer div
+var btn1 = document.getElementById('btn1');
+var botonera = document.getElementById('botonera');
+var anim1 = bodymovin.loadAnimation({
+	container: div1,
+	renderer: 'svg',
+	loop: false,
+	autoplay: false,
+	path: 'pant1.json'
+});
+
+// Cargar la segunda animación en el segundo div
+var div2 = document.getElementById('div2');
+var anim2 = bodymovin.loadAnimation({
+	container: div2,
+	renderer: 'svg',
+	loop: false,
+	autoplay: false,
+	path: 'test.json'
+});
+
+// Reproducir la primera animación cuando se hace clic en el primer div
+btn1.addEventListener('click', function () {
+	btn1.classList.add("hide2");
+	anim1.playSegments([0, 72], true);
+	anim1.addEventListener('complete', function () {
+		anim1.pause();
+	botonera.classList.remove("hide");
+
+	});
+});
+
+// Reproducir la segunda animación cuando se hace clic en el segundo div
+div2.addEventListener('click', function () {
+	anim2.playSegments([50, 100], true);
+});
+
+
+
